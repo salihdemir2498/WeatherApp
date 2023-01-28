2c05969da502dd0821aea724cc15cc990f36e60f
Salih Demir <salihhdemir1727@gmail.com>
2023-01-28 23:06:28 +0300

    react weather app

---
+++ weather-app/src/api.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+import { API_KEY } from "./network/env/config";
+
+export async function fetchWeather(city, setError) {
+
+   
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`;
+
+    try {
+        const response = await axios.get(url);
+        setError("");
+        console.log(response.data);
+        return response.data;
+    } catch (error) {
+        setError("City Not Found!");
+        return error;
+    }
+}
+
+
+
